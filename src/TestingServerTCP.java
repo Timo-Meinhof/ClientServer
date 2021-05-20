701523e3a83fd9ab8cd78c0d7d7eebc6a9741289
Timo Meinhof <67917395+Timo-Meinhof@users.noreply.github.com>
2021-05-20 12:51:03 +0200

    Delete src directory
    updating

--- src/TestingServerTCP.java
+++
@@ -1,9 +0,0 @@
-public class TestingServerTCP {
-
-    public static void main(String[] args) {
-        TCPServer tcpServer = new TCPServer();
-        tcpServer.start(3080);
-
-        tcpServer.stop();
-    }
-}
--- src/TestingServerUDP.java
+++
@@ -1,9 +0,0 @@
-public class TestingServerUDP {
-
-    public static void main(String[] args) {
-        UDPServer udpServer = new UDPServer(3090);
-        udpServer.start();
-
-        udpServer.stopServer();
-    }
-}
